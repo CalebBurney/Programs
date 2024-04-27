24f908b05752cfd3d2059aa09441f2180c66dac9
CalebBurney <168248840+CalebBurney@users.noreply.github.com>
2024-04-26 22:29:06 -0400

    Add files via upload

---
+++ myprog1.py
@@ -0,0 +1,7 @@
+#input data 
+studentName = input('Enter student name. ')
+degreeName = input('Enter degree program. ')
+
+print()
+print('The student/s name is', studentName)
+print('The degree name is', degreeName) 
