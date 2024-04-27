59be5a2e00cdf8bc24099e4418be7988bb46b54d
CalebBurney <168248840+CalebBurney@users.noreply.github.com>
2024-04-26 22:40:01 -0400

    Add files via upload

---
+++ wronganswer.py
@@ -0,0 +1,9 @@
+def main():
+    answer = input('Who is the greatest to ever do it? Enter answer: ')
+
+    if answer == 'caleb':
+        print('you know it!')
+    else: 
+        print('sorry, you are stupid')
+
+main()
